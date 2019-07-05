f4436a3022eb64c498e07787b8224c6c92f9cba0
ien1221 <52518029+ien1221@users.noreply.github.com>
2019-07-05 17:55:01 +0800

    Add files via upload

--- portfolio/labs/lab02/lab.js
+++ portfolio/labs/lab02/lab.js
@@ -1,9 +1,25 @@
 /*
 
 */
+function update(){
+    var x =document.getElementById('in1').value;
+    var ele =document.getElementById('div2');
+    var y =' ';
+    for (var i = 0;i < x.length;i++) {
+        y += x.charAt(i)
+        y += ' U+';
+        y += x.charCodeAt(i).toString(16).toUpperCase();
+        y += '<br>';
+    }
+    ele.innerHTML = y;
 
+    document.getElementById('div1').innerHTML =
+    document.getElementById('in1').value.replace('我回來了','你回來了')
+}
 
 function start(e) {
+    document.getElementById('in1').addEventListener("input",update);
+    update();
 
 }
 
